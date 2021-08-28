8b93018a6b0ee39eb6e6c7c7d42790b1b0af3232
fajarslvn <jarphish@gmail.com>
2021-08-28 17:51:06 +0700

    add task component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,36 @@
+import { useState } from 'react'
 import Header from "./Header"
+import Tasks from './Tasks'
 
 const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Doctors Appointment',
+      day: 'Feb 5th at 2:30pm',
+      reminder: true,
+    },
+    {
+     id: 2,
+     text: 'Client Meeting',
+     day: 'Feb 6th at 10:00am',
+     reminder: true,
+   },
+   {
+     id: 3,
+     text: 'Lunch',
+     day: 'Feb 7th at 1:00pm',
+     reminder: false,
+   },
+ ])
+
   return (
     <div className="container">
       <Header />
+      <Tasks tasks={tasks}/>
     </div>
   )
 }
 
 export default App
+ 
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,15 @@
 import PropTypes from 'prop-types';
 
-const Button = ({ color, text }) => {
+const Button = ({ color, text, onClick }) => {
   return (
-    <button style={{ backgroundColor: color }} className="btn">
+    <button onClick={onClick} 
+    style={{ backgroundColor: color }} 
+    className="btn">
       {text}
     </button>
   );
 };
-
+ 
 Button.defaultProps = {
   text: 'Change me',
   color: 'steelblue ',
@@ -16,6 +18,7 @@ Button.defaultProps = {
 Button.propTypes = {
   text: PropTypes.string,
   color: PropTypes.string,
+  onClick: PropTypes.func,
 };
 
 export default Button;
